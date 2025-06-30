a0597aecca9f08d2d8f8846dd8380dd8e01bdca7
Roel Devv <johnroelf17@gmail.com>
2025-07-01 03:15:11 +0800

    textpressure

--- src/app/components/text-pressure/TextPressure.tsx
+++ src/app/components/text-pressure/TextPressure.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import { useEffect, useRef, useState, useCallback } from "react";
 
 interface TextPressureProps {
@@ -207,7 +209,9 @@ const TextPressure: React.FC<TextPressureProps> = ({
         {chars.map((char, i) => (
           <span
             key={i}
-            ref={(el) => (spansRef.current[i] = el)}
+            ref={(el) => {
+              spansRef.current[i] = el;
+            }}
             data-char={char}
             className="inline-block"
           >
